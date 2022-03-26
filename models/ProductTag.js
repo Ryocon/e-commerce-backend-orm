874a147b7c8dce3d8e9c393f8dde21d8d82553ae
roconn <roconn25@gmail.com>
2022-03-26 20:22:46 +0000

    added to product_tags model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,6 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'tag',
+          key: 'id'
+        }
+      }
+    }
     // define columns
   },
   {
@@ -18,3 +38,5 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+// DONE !!!
